3a8749ab1e76e8e3c6846456e019b9717db16c45
leedongkyu <leedong0225@icloud.com>
2023-09-04 18:47:21 +0900

    projectPush

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { I18n } from 'i18n-js';
+import { getLocales } from 'expo-localization';
+
+const i18n = new I18n({
+    en:{welcome: 'hello' },
+    ja:{welcome: "新しい出発を待っています！"}});
+
+const deviceLanguage = getLocales()[0].languageCode;
+i18n.locale = deviceLanguage;
+
+console.log(i18n.t('welcome'));
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{deviceLanguage}</Text>
       <StatusBar style="auto" />
     </View>
   );
